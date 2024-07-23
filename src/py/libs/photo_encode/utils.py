bef2784556a82a1516cfbad65b9d906d8c213564
Jake Bauer <jake.bauer.2.71828@gmail.com>
2024-07-23 00:14:41 -0400

    Fixed .gitignore and implemented dino embedding loop

--- src/py/libs/photo_encode/utils.py
+++ src/py/libs/photo_encode/utils.py
@@ -10,7 +10,11 @@ from PIL import Image
 
 
 def image_batch_from_folder_generator(
-    folder: str, transform: Callable[Image, torch.Tensor], batch_size: int
+    folder: str,
+    transform: Callable[Image, torch.Tensor],
+    transform_kwargs: dict = None,
+    batch_size: int = 8,
+    collate: Callable[torch.Tensor, torch.Tensor] = None,
 ) -> (torch.Tensor, List[str]):
     """
     Loads images from a folder as tensor batches until the
@@ -22,8 +26,12 @@ def image_batch_from_folder_generator(
       The local folder to pull images from
     transform : Callable
       Produces a torch tensor from PIL images. For example, a Compose transform.
+    transform_kwargs : dict
+      If needed, kwargs to pass to the transform.
     batch_size : int
       The batch size
+    collate : Callable[torch.Tensor, torch.Tensor]
+      How to collate images into a batch. Defaults to stack.
 
     Yields
     ------
@@ -33,8 +41,18 @@ def image_batch_from_folder_generator(
     batch_files : List[str]
       The filenames in the batch
     """
+    transform_kwargs = transform_kwargs or {}
+
     image_files = [os.path.join(folder, f) for f in os.listdir(folder)]
     for i in range(0, len(image_files), batch_size):
         batch_files = image_files[i : i + batch_size]
-        images = [transform(Image.open(f).convert("RGB")) for f in batch_files]
-        yield torch.stack(images), batch_files
+        images = [
+            transform(Image.open(f).convert("RGB"), **transform_kwargs)
+            for f in batch_files
+        ]
+
+        # Create batch
+        if collate is None:
+            yield torch.stack(images), batch_files
+        else:
+            yield collate(images), batch_files
